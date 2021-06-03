82b822ca168f5f36f62c51ab5af5d0857e39fb9b
유트랜스 <utrans77@naver.com>
2021-06-03 14:16:31 +0000

    나도코딩 실습 1차 완료

--- index.py
+++ index.py
@@ -151,3 +151,130 @@
 #         break
 
 # (5:14:23) 11-1.모듈
+
+# import theather_module
+# theather_module.price(3)
+# theather_module.price_morning(4)
+# theather_module.price_soldier(5)
+
+# import theather_module as mv
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# from theather_module import *
+
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+# from theather_module import price, price_morning
+
+# price(5)
+# price_morning(6)
+# price_soldier(7)
+
+# from theather_module import price_soldier as price
+# price(5)
+
+# (5:24:10) 11-2.패키지
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# (5:30:30) 11-3._all_
+# from random import *
+
+# from travel import *
+# # trip_to = vietnam.VietnamPackage()
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
+
+# # (5:34:16) 11-4.모듈 직접 실행
+# from travel import *
+# # # trip_to = vietnam.VietnamPackage()
+# # trip_to = thailand.ThailandPackage()
+# # trip_to.detail()
+
+# # (5:37:00) 11-5.패키지, 모듈 위치
+# import inspect
+# import random
+# print(inspect.getfile(random))
+# print(inspect.getfile(thailand))
+
+# (5:40:33) 11-6.pip install
+
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+# print(soup.prettify())
+
+# (5:46:04) 11-7.내장함수
+# input : 사용자 입력을 받는 함수
+# language = input("무슨 언어를 좋아하세요?")
+# print('{}은 아주 좋은 언어입니다.'.format(language))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고  있는지 표시
+# print(dir())
+# import random # 외장 함수
+# print(dir())
+# import pickle
+# print(dir())
+
+# print(dir(random))
+
+# lst = [1,2,4]
+# print(dir(lst))
+
+# name = "jim"
+# print(dir(name))
+
+# (5:50:38) 11-8.외장함수
+# glob : 경로 내의 폴더 / 파일 목록 조회(윈도우 dir)
+# import glob
+# print(glob.glob("*.py")) # 확장자가 py 인 모든  파일
+
+# os : 운영체제에서 제공하는 기본 기능
+# import os
+# print(os.getcwd()) # 현재 디렉토리
+
+# folder = "sample_dir"
+
+# if os.path.exists(folder):
+#     print("이미 존재하는 폴더입니다.")
+#     os.rmdir(folder)
+#     print(folder,"폴더를 삭제하였습니다.")
+# else:
+#     os.makedirs(folder) # 폴더 생성
+#     print(folder, "폴더를 생성하였습니다.")
+# print(os.listdir())
+
+# time : 시간 관련 함수
+# import time
+# print(time.localtime())
+# print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+# import datetime
+# # print("오늘 날짜는", datetime.date.today())
+
+# # timedelta : 두 날짜 사이의 간격
+# today = datetime.date.today()
+# td = datetime.timedelta(days=100)
+# print("우리가 만난지 100일은", today + td)
+
+# (5:58:49) 11-9.퀴즈 #10
+# 프로젝트 내에 나만의  시그니처를 남기는 모듈을 만드시오
+# 조건 : 모듈 파일명은 byme.py 로 작성
+# (모듈 사용 예제)
+# import byme
+# byme.sign()
+
+# (출력예제)
+# 이 프로그램은 나도코딩에 의해 만들어졌습니다.
+# 유튜브 : http://youtube.com
+# 이메일 : nadocoding@gmail.com
+
+# (6:01:08) 12.Outro
