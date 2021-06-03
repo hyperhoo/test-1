4864b71fccf956327643c30d828987af1310ae84
유트랜스 <utrans77@naver.com>
2021-06-03 10:00:55 +0000

    나도코딩 파이썬 내용 추가

--- index.py
+++ index.py
@@ -1,135 +1,29 @@
-from unittest import TestCase
-import unittest
+# 동영상 (4:50:13) 10-1.예외처리
 
+# try:    
+#     print("나누기 전용 계산기입니다.")    
+#     num1 = int(input("첫 번째 숫자를 입력하세요 :"))
+#     num2 = int(input("두 번째 숫자를 입력하세요 :"))
+#     print("{} / {} = {}".format(num1, num2, int(num1/num2)))
 
-class FieldElement:
+# except ValueError:
+#     print("에러! 잘못된 값을 입력하였습니다.")
+# except ZeroDivisionError as err:
+#     print(err)
 
-    def __init__(self, num, prime):
-        if num >= prime or num < 0:
-            error = 'Num {} not in field range 0 to {}'.format(
-                num, prime-1
-            )
-            raise ValueError(error)
-        self.num = num
-        self.prime = prime
+try:    
+    print("나누기 전용 계산기입니다.")    
+    nums = []
+    nums.append(int(input("첫 번째 숫자를 입력하세요 :")))
+    nums.append(int(input("두 번째 숫자를 입력하세요 :")))
+    # nums.append(int(nums[0]/nums[1]))
+    print("{} / {} = {}".format(nums[0], nums[1], nums[2]))
 
-    def __repr__(self):
-        return 'FieldElement_{}({})'.format(self.prime, self.num)
+except ValueError:
+    print("에러! 잘못된 값을 입력하였습니다.")
+except ZeroDivisionError as err:
+    print(err)
+except:
+    print("알 수 없는 에러가 발생하였습니다.")
 
-    def __eq__(self, other):
-        if other is None:
-            return False
-        return self.num == other.num and self.prime == other.prime
-
-    def __ne__(self, other):
-        return not(self == other)
-
-    def __add__(self, other):
-        if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in different Fields')
-        num = (self.num + other.num) % self.prime
-        return self.__class__(num, self.prime)
-
-    def __sub__(self, other):
-        if self.prime != other.prime:
-            raise TypeError('Cannot subtract two numbers in different Fields')
-        num = (self.num - other.num) % self.prime
-        return self.__class__(num, self.prime)
-
-    # 연습문제 1.6
-    def __mul__(self, other):
-        if self.prime != other.prime:
-            raise TypeError('Cannot multiply two numbers in different Fields')
-        num = (self.num * other.num) % self.prime
-        return self.__class__(num, self.prime)
-
-    def __pow__(self, exponent):
-        num = (self.num ** exponent) % self.prime
-        return self.__class__(num, self.prime)
-
-
-class FieldElementTest(TestCase):
-
-    # page 42
-    def test1(self):
-        a = FieldElement(7, 13)
-        b = FieldElement(6, 13)
-        print(a == b)
-        print(a == a)
-
-    # page 49
-    def test2(self):
-        a = FieldElement(7, 13)
-        b = FieldElement(12, 13)
-        c = FieldElement(6, 13)
-        print(a+b == c)
-
-    # 연습문제 1.4
-    def test3(self):
-        prime = 97
-        print((95 * 45 * 31) % prime)
-        print((17 * 13 * 19 * 44) % prime)
-        print((12**7 * 77**49) % prime)
-
-    # 연습문제 1.5
-    def test4(self):
-        prime = 19
-        k = [1, 3, 7, 13, 18]
-        member = []
-        for i in k:
-            for j in range(0, 19):
-                member.append((i*j) % prime)
-            member.sort()
-            print(member)
-            member.clear()
-
-    # page 50
-    def test5(self):
-        a = FieldElement(3, 13)
-        b = FieldElement(1, 13)
-        print(a**3 == b)
-
-    # 연습문제 1.7 - 서정훈 풀이
-    def test6(self):
-        p = 7
-        member = []
-        for j in range(1, 7):
-            member.append((j**(7-1)) % 7)
-        member.sort()
-        print(member)
-        member.clear()
-
-        p = 11
-        member = []
-        for j in range(1, 11):
-            member.append((j**(11-1)) % 11)
-        member.sort()
-        print(member)
-        member.clear()
-
-        p = 17
-        member = []
-        for j in range(1, 17):
-            member.append((j**(17-1)) % 17)
-        member.sort()
-        print(member)
-        member.clear()
-
-        p = 31
-        member = []
-        for j in range(1, 31):
-            member.append((j**(31-1)) % 31)
-        member.sort()
-        print(member)
-        member.clear()
-
-    # 연습문제 1.7 - 책 해답
-    def test7(self):
-        for prime in (7, 11, 17, 31):
-            print([pow(i, prime-1, prime) for i in range(1, prime)])
-
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    # runner.run(unittest.makeSuite(FieldElementTest, 'test6'))
-    runner.run(unittest.makeSuite(FieldElementTest, 'test7'))
-    
+# (4:58:15) 10-2.에러 발생시키기
---
+++ python_training.py
@@ -0,0 +1,5 @@
+print("나누기 전용 계산기입니다.")
+num1 = int(input("첫 번째 숫자를 입력하세교 :"))
+num2 = int(input("두 번째 숫자를 입력하세교 :"))
+print("{} / {} = {}".format(num1, num2, int(num1/num2)))
+
